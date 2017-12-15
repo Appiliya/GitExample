46a21eda38dada1323638ce7fdce061561828d5e
Appiliya <Allf@outlook.com>
2017-12-15 21:03:04 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/appil/gitexample/GitExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.appil.gitexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitExampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_example);
+    }
+}
